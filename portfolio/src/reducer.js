45375b92aa83f01afdbbf7195a1d5e51a8a7ec86
teej387 <teej387@yahoo.com>
2019-08-04 13:31:25 -0500

    Created Home component

---
+++ portfolio/src/reducer.js
@@ -0,0 +1,11 @@
+/* import action types from actions.js */
+
+const initialState = {
+
+}
+
+// export default function(state=initialState, action) {
+//   switch (action.type) {
+//     case 
+//   }
+// }
