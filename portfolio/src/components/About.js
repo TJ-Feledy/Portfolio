9fbb95732d0a58e6cf4a35ee75aef5aabeb89796
teej387 <teej387@yahoo.com>
2019-08-04 20:44:00 -0500

    Created About component

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class About extends React.Component {
+
+
+  render() {
+    return (
+      <div className='About'>
+        <h1 className='aboutHeader'>ABOUT</h1>
+        <i class="fas fa-wave-square"></i>
+        <div className='aboutCards'>
+          <div className='aboutCard'>
+            <i class="fas fa-globe"></i>
+            <h2 className='aboutH2'>Intuitive</h2>
+            <p className='aboutP'>Being user friendly and ease of navigation are top priorities.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
