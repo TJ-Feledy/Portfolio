123cf234f9b5e6d3af375bf565c6306b2642d48f
teej387 <teej387@yahoo.com>
2019-08-04 20:10:55 -0500

    Imported Route in app for Home component

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className='home'>
-
+        <p className='hello'>Hello, my name is <span className='name'>Theodore Feledy.</span></p>
       </div>
     )
   }
