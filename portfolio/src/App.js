ad46eb5e6e95c312367f3b743a45005ce069ec08
teej387 <teej387@yahoo.com>
2019-08-18 14:57:19 -0500

    Fixed some bugs

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Route path='/' render={props => <Home {...props} /> } />
+      <Route path='/' render={props => <About {...props} /> } />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class About extends React.Component {
+
+
+  render() {
+    return (
+      <div className='About' id='About'>
+        <h1 className='aboutHeader'>ABOUT</h1>
+        <i className="fas fa-wave-square fa-lg"></i>
+        <div className='aboutCards'>
+          <div className='aboutCard'>
+            <div className='iconWrapper'><i className="fas fa-globe fa-4x"></i></div>
+            <h2 className='aboutH2'>Intuitive</h2>
+            <p className='aboutP'>User friendly design and ease of navigation are top priorities.</p>
+          </div>
+          <div className='aboutCard'>
+            <div className='iconWrapper'><i className="fas fa-heartbeat fa-4x"></i></div>
+            <h2 className='aboutH2'>Dynamic</h2>
+            <p className='aboutP'>I love breathing life into a webpage.</p>
+          </div>
+          <div className='aboutCard'>
+            <div className='iconWrapper'><i className="fas fa-expand-arrows-alt fa-4x"></i></div>
+            <h2 className='aboutH2'>Responsive</h2>
+            <p className='aboutP'>Order at all levels. Creating a responsive design is what I do.</p>
+          </div>
+          <div className='aboutCard'>
+            <div className='iconWrapper'><i className="fas fa-cog fa-4x fa-spin"></i></div>
+            <h2 className='aboutH2'>Functional</h2>
+            <p className='aboutP'>If you can dream it, I can make it work.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default About
