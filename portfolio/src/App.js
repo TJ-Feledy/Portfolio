a191626227f05a18accfc3528a8be6ba2fe7ef0d
teej387 <teej387@yahoo.com>
2020-02-21 01:50:28 -0600

    Changed font, styled aboutCards and added animation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
+import Home from './components/Home.js'
+import About from './components/About.js'
 
 function App() {
   return (
