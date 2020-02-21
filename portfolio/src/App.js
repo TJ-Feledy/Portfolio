5a44cdb94a9abc2f1e7c7df43b16d9e8002861d8
teej387 <teej387@yahoo.com>
2020-02-21 14:13:42 -0600

    Adjusted animation and refactored routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home.js'
 import About from './components/About.js'
 
 function App() {
   return (
     <div className="App">
-      
-      <Route path='/' render={props => <Home {...props} /> } />
-      <Route path='/' render={props => <About {...props} /> } />
+      <Switch>
+        <Route exact path='/' render={props => <Home {...props} /> } />
+        <Route path='/about' render={props => <About {...props} /> } />
+      </Switch>
     </div>
   );
 }
--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
 
-  aboutClick = evt => {
-    evt.preventDefault()
-    document.getElementById('About').scrollIntoView(true)
-  }
+  // aboutClick = evt => {
+  //   evt.preventDefault()
+  //   document.getElementById('About').scrollIntoView(true)
+  // }
 
   render() {
     return (
       <div className='home'>
         <p className='hello'>Hello, my name is <span className='name'>Theodore Feledy.</span></p>
-        <h3 className='homeLink' onClick={this.aboutClick} >Let's get started! <i className="fas fa-caret-right"></i></h3>
+        <Link to='/about' className='aboutLink'>
+          <h3 className='homeLink' >Let's get started! <i className="fas fa-caret-right"></i></h3>
+        </Link>
         <p className='hello'>I'm a Full Stack Web Developer.</p>
       </div>
     )
