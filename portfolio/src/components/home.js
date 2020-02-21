68f8b70502a6b40117f868c2fcdd156fc90ee0d5
teej387 <teej387@yahoo.com>
2020-02-21 15:13:35 -0600

    home bug

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
 
 class Home extends React.Component {
+
+  aboutClick = evt => {
+    evt.preventDefault()
+    document.getElementById('About').scrollIntoView(true)
+  }
+
   render() {
     return (
-      <div className="home">
-        <p className="hello">
-          Hello, my name is <span className="name">Theodore Feledy.</span>
-        </p>
-        <Link to="/about" className="aboutLink">
-          <h3 className="homeLink">
-            Let's get started! <i className="fas fa-caret-right"></i>
-          </h3>
-        </Link>
-        <p className="hello">I'm a Full Stack Web Developer.</p>
+      <div className='home'>
+        <p className='hello'>Hello, my name is <span className='name'>Theodore Feledy.</span></p>
+        <h3 className='homeLink' onClick={this.aboutClick} >Let's get started! <i className="fas fa-caret-right"></i></h3>
+        <p className='hello'>I'm a Full Stack Web Developer.</p>
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
