a862db1655417513f9c752a16816b10f326af754
teej387 <teej387@yahoo.com>
2020-02-23 01:12:56 -0600

    Created Skills.js - added header

---
+++ portfolio/src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class Skills extends React.Component {
+  render() {
+    return (
+      <div className='Skills'>
+        <h1 className='skillsHeader'>ABOUT/TECH-STACK</h1>
+        <div className='iconContainer'>
+          <i className="fas fa-wave-square fa-lg"></i>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default Skills
