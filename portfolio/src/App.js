b84223e82c684050bb551dd51a0d7a41bd152649
teej387 <teej387@yahoo.com>
2020-02-22 18:14:23 -0600

    Fixed CSS.registerProperty bug

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch, withRouter } from 'react-router-dom'
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 import Home from './components/Home.js'
 import About from './components/About.js'
 import Nav from './components/Nav.js'
 
-function App() {
+function App({ location }) {
 
-  window.CSS.registerProperty({
-    name: '--y',
-    syntax: '<length-percentage>',
-    initialValue: '20%',
-    inherits: true
-  })
-  window.CSS.registerProperty({
-    name: '--x',
-    syntax: '<length-percentage>',
-    initialValue: '20%',
-    inherits: true
-  })
+  React.useEffect(() => {
+    window.CSS.registerProperty({
+      name: '--y',
+      syntax: '<length-percentage>',
+      initialValue: '20%',
+      inherits: true
+    })
+    window.CSS.registerProperty({
+      name: '--x',
+      syntax: '<length-percentage>',
+      initialValue: '20%',
+      inherits: true
+    })
+  }, [])
 
   return (
     <div className="App">
       <Nav />
-      <Switch>
-        <Route exact path='/' render={props => <Home {...props} /> } />
-        <Route path='/about' render={props => <About {...props} /> } />
-      </Switch>
+      <TransitionGroup>
+        <CSSTransition 
+          key={location.key} 
+          timeout={{ enter: 300, exit: 300 }}
+          classNames={'fade'}
+        >
+          <Switch location={location}>
+            <Route exact path='/' render={props => <Home {...props} /> } />
+            <Route path='/about' render={props => <About {...props} /> } />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
