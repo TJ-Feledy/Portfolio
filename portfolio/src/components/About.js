59cc194b387a3fd495992a308c4b32c2d3bfa52a
teej387 <teej387@yahoo.com>
2020-02-24 17:56:39 -0600

    Finished bio

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import me from '../images/me.jpg'
 import reduxImg from '../images/redux.png'
+import lambda from '../images/lambda-logo.png'
+import sqlite from '../images/sqlite.png'
 
 
 class About extends React.Component {
@@ -34,18 +36,28 @@ class About extends React.Component {
                 <h2 className='techHeader'>Back-End</h2>
                 <div className='techInfoContainer'>
                   <div className='languageContainer'>
-
+                    <p className='techName'><i class="fab fa-node-js techIcon" /> - NODE</p>
+                    <p className='techName'><i class="fab fa-node-js techIcon" /> - EXPRESS</p>
+                    <p className='techName'><img className="techIcon sqlite" src={sqlite} /> - SQLITE3</p>
                   </div>
-
                 </div>
               </div>
             </div>
           </div>
 {/* My picture/bio */}
           <div className='Me'>
+            <h2 className='stackHeader bioHeader'>Bio</h2>
+            <p className='backInFive'>.<br />Back<br /> in 5!</p>
             <div className='meWrapper'>
               <div className='meContainer'>
                 <img src={me} className='meImg' alt='Handsome Man' />
+              </div>
+            </div>
+            <div className='bioTextContainer'>
+              <div className='bioText'>
+                <span className='bioLine'>I am a Web Developer currently enrolled at <a className='lambdaLink' href='https://lambdaschool.com/courses/full-stack-web-development'><img className="lambda" src={lambda} />Lambda School</a>.</span>
+                <span className='bioLine'>I love to create something I can be proud of, to meet new people, and to learn new things.</span>
+                <span className='bioLine'>For me, this isn't just work... I enjoy the challenge that each project brings.</span>
               </div>
             </div>
           </div>
