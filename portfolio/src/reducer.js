0604c8f683f5a0e14fa8fa8f056af285cd6088be
TJ-Feledy <teej387@yahoo.com>
2020-06-20 09:31:26 -0500

    imported action types to reducer

--- portfolio/src/reducer.js
+++ portfolio/src/reducer.js
@@ -1,4 +1,15 @@
 /* import action types from actions.js */
+import {
+    INITIALIZE_COUNT_START,
+    INITIALIZE_COUNT_SUCCESS,
+    INITIALIZE_COUNT_FAILED,
+    UPDATE_COUNT_START,
+    UPDATE_COUNT_SUCCESS,
+    UPDATE_COUNT_FAILED,
+    GET_COUNT_START,
+    GET_COUNT_SUCCESS,
+    GET_COUNT_FAILED
+} from './actions.js'
 
 const initialState = {
     isLoading: false,
