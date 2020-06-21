8f88146b06509255bb2f75d768f96f32a2f4d233
TJ-Feledy <teej387@yahoo.com>
2020-06-21 08:51:29 -0500

    Created counter component

---
+++ portfolio/src/components/Counter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter = {}
+        }
+    }
+
+    componentDidMount {
+        this.setState({
+            counter: this.props.counter
+        })
+    }
+
+    render() {
+        return (
+            <div className='Counter'>
+                <div className='eyeContainer'><i className="eye far fa-eye"></i></div>
+                <p className='count'>this.state.counter.count</p>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    counter: state.counter,
+    isLoading: state.isLoading
+})
+
+export default connect(mapStateToProps)(Counter)
