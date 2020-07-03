37ecc2c286338a0c063a87fbf9296b5bbf32cac9
TJ-Feledy <teej387@yahoo.com>
2020-07-03 09:16:55 -0500

    Added resume pdf

--- portfolio/src/components/NetworkLinks.js
+++ portfolio/src/components/NetworkLinks.js
@@ -5,6 +5,7 @@ function NetworkLinks() {
         <div className='networksContainer'>
           <a className='myGitLink' href='https://github.com/TJ-Feledy'>-GitHub-</a>
           <a className='myLinkedInLink' href='https://www.linkedin.com/in/theodore-feledy/'>-LinkedIn-</a>
+          <a className='myResumeLink' href='../resources/resume.pdf'>-Resume-</a>
         </div>
     )
 }
