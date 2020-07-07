9c7117aef837d7b84393f3192529224799a8a2d9
TJ-Feledy <teej387@yahoo.com>
2020-07-06 20:13:25 -0500

    Created Contact component

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -62,7 +62,7 @@ class About extends React.Component {
             <div className='bioTextContainer'>
               <p className='bioName'>Theodore J. Feledy</p>
               <div className='bioText'>
-                <span className='bioLine'>I am a Web Developer currently enrolled at <a className='lambdaLink' href='https://lambdaschool.com/courses/full-stack-web-development'><img className="lambda" src={lambda} alt='' />Lambda School</a>.</span>
+                <span className='bioLine'>I am a Web Developer who studied at <a className='lambdaLink' href='https://lambdaschool.com/courses/full-stack-web-development'><img className="lambda" src={lambda} alt='' />Lambda School</a>.</span>
                 <span className='bioLine'>I love to create something I can be proud of, to meet new people, and to learn new things.</span>
                 <span className='bioLine'>For me, this isn't just work... I enjoy the challenges that each new project brings.</span>
               </div>
