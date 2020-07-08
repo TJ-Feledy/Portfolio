a6deddfda2b913f710a2d15c8939afdbe0435673
TJ-Feledy <teej387@yahoo.com>
2020-07-07 23:45:24 -0500

    Formatted and added style to contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Counter from './Counter.js'
+import NetworkLinks from './NetworkLinks.js'
+
 class Contact extends React.Component {
     constructor(props) {
         super(props)
@@ -20,29 +23,31 @@ class Contact extends React.Component {
                     <i className="fas fa-wave-square fa-lg"></i>
                 </div>
                 <form className='contactForm'>
-                    <div className='contactName'>
+                    <div id='contactDiv' className='contactName'>
                         <label className='contactNameLabel' htmlFor='name'>Full Name: </label>
-                        <input className='contactNameInput' type='text' name='name' id='name' />
+                        <input className='contactNameInput' required type='text' name='name' id='name' />
                     </div>
-                    <div className='contactCompany'>
+                    <div id='contactDiv' className='contactCompany'>
                         <label className='contactCompanyLabel' htmlFor='company'>Company: </label>
                         <input className='contactCompanyInput' type='text' name='company' id='company' />
                     </div>
-                    <div className='contactEmail'>
+                    <div id='contactDiv' className='contactEmail'>
                         <label className='contactEmailLabel' htmlFor='email'>Email: </label>
-                        <input className='contactEmailInput' type='email' placeholder='janeDoe@gmail.com' name='email' id='email' />
+                        <input className='contactEmailInput' required type='email' placeholder='janeDoe@gmail.com' name='email' id='email' />
                     </div>
-                    <div className='contactPhone'>
+                    <div id='contactDiv' className='contactPhone'>
                         <label className='contactPhoneLabel' htmlFor='phone'>Phone Number: </label>
                         <input className='contactPhoneInput' type='tel' placeholder='12195551234' name='phone' id='phone' />
                     </div>
-                    <div className='contactBody'>
+                    <div id='contactDiv' className='contactBody'>
                         <label className='contactBodyLabel' htmlFor='body'>Please Leave a Message: </label>
-                        <textarea className='contactBodyTextarea' placeholder='Whether it is for work or a criticism, I would love to here what you have to say.' name='body' id='body' />
+                        <textarea className='contactBodyTextarea' required placeholder='    Whether it is for work or a criticism, I would love to hear what you have to say.' name='body' id='body' />
                     </div>
-                    <button type='submit'>Send Me Your Info!</button>
+                    <button className='contactButton' type='submit'>Send Me Your Info!</button>
                 </form>
                 <p className='contactThankyou'>Thank you, and I look forward to hearing from you!</p>
+                <Counter />
+                <NetworkLinks />
             </div>
         )
     }
